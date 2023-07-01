fd11f1f9763844b5e304cf7fa767bfb285f17316
Camille Fassett <camillefassett@gmail.com>
2023-07-01 10:27:24 -0700

    update setup script to include package management, ssh, and python setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,51 @@
+# check if Homebrew is installed and install if not
+if test ! $(which brew); then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# install VSCode, iTerm2, pipenv if not exists
+if test ! $(which code); then
+    echo "Installing VSCode..."
+    brew install --cask visual-studio-code
+fi
+
+
+if test ! $(which iterm); then
+    echo "Installing iTerm2..."
+    brew install --cask iterm2
+fi
+
+if test ! $(which pipenv); then
+    echo "Installing pipenv..."
+    brew install pipenv
+fi
+
+# install pip and jupyter notebooks if not exists
+if test ! $(which pip); then
+    echo "Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+fi
+
+if test ! $(which jupyter); then
+    echo "Installing Jupyter..."
+    python -m pip install jupyter
+    python -m ipykernel install --user
+fi
+
+# install Rust if not exists
+if test ! $(which rustc); then
+    echo "Installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
 DOTFILES_DIRECTORY=~/dotfiles
 VIMRC_FILE="$DOTFILES_DIRECTORY/vim/.vimrc"
 ZSHRC_FILE="$DOTFILES_DIRECTORY/zsh/.zshrc"
 STARSHIP_FILE="$DOTFILES_DIRECTORY/config/starship.toml"
 
-# Function to create symlink
+# function to create symlink
 create_symlink() {
     if [ -L $2 ]; then
         echo "Removing existing symlink $2"
@@ -17,9 +59,59 @@ create_symlink() {
     ln -s $1 $2
 }
 
-# Create symlinks
+# create symlinks
 create_symlink $VIMRC_FILE ~/.vimrc
 create_symlink $ZSHRC_FILE ~/.zshrc
 create_symlink $STARSHIP_FILE ~/.config/starship.toml
 
 echo "Symlink creation complete"
+
+# pyenv python
+# set desired python version  
+PYTHON_VERSION="3.8.10"
+
+# check if pyenv is installed
+command -v pyenv >/dev/null 2>&1 || { echo >&2 "Pyenv is not installed. Installing it now..."; curl https://pyenv.run | bash; }
+
+# make sure .zshrc has necessary pyenv configuration
+if ! grep -q 'pyenv init' ~/.zshrc; then
+   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+   echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.zshrc
+   echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+   source ~/.zshrc
+fi
+
+# check if the desired Python version is installed via pyenv
+if ! pyenv versions --bare | grep -q $PYTHON_VERSION; then
+    echo "Python $PYTHON_VERSION not found in pyenv versions. Installing now..."
+    pyenv install $PYTHON_VERSION
+fi
+
+# set the desired Python version as the global version
+pyenv global $PYTHON_VERSION
+
+# custom terminal font
+TARGET_FONT_DIR="/Library/Fonts"
+
+# check if font source directory exists
+if [ -d "~/dotfiles/iterm2/JetBrainsMono" ]; then
+    echo "Source directory found. Proceeding with copying files..."
+else
+    echo "Source directory not found. Please check the path and try again."
+    exit 1
+fi
+
+# copy JetBrainsMono fonts to target directory
+sudo cp -R "~/dotfiles/iterm2/JetBrainsMono/*" "$TARGET_FONT_DIR"
+
+# generate SSH key pair if not exists 
+SSH_DIR="$HOME/.ssh"
+SSH_KEY="$SSH_DIR/id_ed25519"
+
+if [ ! -f "$SSH_KEY" ]; then
+    echo "SSH key not found. Generating a new one..."
+    ssh-keygen -t ed25519 -C "camille@sempervirens.io"
+else
+    echo "SSH key already exists."
+fi
